9deb887eafa668b3194c09cb6b0a7e7e6e09f0bb
reda-coder <redaelouahabi98@gmail.com>
2021-04-14 15:14:17 +0000

    commit

--- src/routes/routing.jsx
+++ src/routes/routing.jsx
@@ -7,14 +7,7 @@ import FirstDashboard from '../views/starter/starter.jsx';
 /*--------------------------------------------------------------------------------*/
 // import Alerts from '../views/ui-components/alert.jsx';
 import LogInPage from '../views/ui-components/LogIn.jsx';
-
 import BootstrapDocs from '../views/ui-components/bootstrapDocs.jsx';
-// import Buttons from '../views/ui-components/Register.jsx';
-// import Cards from '../views/ui-components/cards.jsx';
-// import LayoutComponent from '../views/ui-components/layout.jsx';
-// import PaginationComponent from '../views/ui-components/pagination.jsx';
-// import PopoverComponent from '../views/ui-components/popover.jsx';
-// import TooltipComponent from '../views/ui-components/tooltip.jsx';
 import RegisterPage from '../views/ui-components/Register.jsx';
 import AddLogement from '../views/ui-components/AddLogement.jsx';
 
@@ -43,7 +36,26 @@ var ThemeRoutes = [
     icon: 'mdi mdi-toggle-switch',
     component: RegisterPage
   },
-  // {
+ 
+  {
+    path: '/ui-components/bootstrapDocs',
+    name: 'BootstrapDocs',
+
+    icon: 'mdi mdi-react',
+    component: BootstrapDocs
+  },
+  {
+    path: '/',
+    pathTo: '/starter/starter',
+    name: 'Dashboard',
+    redirect: true
+  }
+  
+];
+export default ThemeRoutes;
+
+
+ // {
   //   path: '/ui-components/card',
   //   name: 'Contact',
   //   icon: 'mdi mdi-credit-card-multiple',
@@ -76,19 +88,3 @@ var ThemeRoutes = [
   //   icon: 'mdi mdi-image-filter-vintage',
   //   component: TooltipComponent
   // },
-  {
-    path: '/ui-components/bootstrapDocs',
-    name: 'BootstrapDocs',
-
-    icon: 'mdi mdi-react',
-    component: BootstrapDocs
-  },
-  {
-    path: '/',
-    pathTo: '/starter/starter',
-    name: 'Dashboard',
-    redirect: true
-  }
-  
-];
-export default ThemeRoutes;
