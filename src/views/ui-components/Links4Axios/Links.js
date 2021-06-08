c03ae68d94f99661ce4f8f1858d8751de0d373ee
reda-coder <redaelouahabi98@gmail.com>
2021-06-08 16:56:28 +0100

    change API URL

--- src/views/ui-components/Links4Axios/Links.js
+++ src/views/ui-components/Links4Axios/Links.js
@@ -2,15 +2,20 @@
 //console.log(links[1].linkPost) donne les 2emes liens : localhost://8082 eclipse
 
 const links = [
-    {
-        key: 1,
-        linkGet: "https://logement-app-api.herokuapp.com/logements/",
-        linkPost: "https://logement-app-api.herokuapp.com/logement/"
-    },
-    {
-        key: 2,
-        linkGet: "http://localhost:8082/logements/",
-        linkPost: "http://localhost:8082/logement/"
-    }
+  {
+    key: 1,
+    linkGet: "https://logeement-app-api.herokuapp.com/logements/",
+    linkPost: "https://logeement-app-api.herokuapp.com/logement/",
+  },
+  // {
+  //     key: 1,
+  //     linkGet: "https://logement-app-api.herokuapp.com/logements/",
+  //     linkPost: "https://logement-app-api.herokuapp.com/logement/"
+  // },
+  {
+    key: 2,
+    linkGet: "http://localhost:8082/logements/",
+    linkPost: "http://localhost:8082/logement/",
+  },
 ];
 export default links;
